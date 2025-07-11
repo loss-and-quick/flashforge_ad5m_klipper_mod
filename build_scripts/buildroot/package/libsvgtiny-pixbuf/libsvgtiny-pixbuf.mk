5d4a68d417ea27185f5bb6a238dacfe78ecfec5b
minicx <minicx@disroot.org>
2025-07-11 17:21:35 +0300

    libsvgtiny-pixbuf: fix compilation and refine Buildroot package
    This commit introduces a patch to **`libsvgtiny-pixbuf`** (`0001-io-svg-fix-error-code.patch`) that resolves a compilation error. Specifically, it replaces the undefined error code `svgtiny_LIBDOM_ERROR` with the correct `svgtiny_LIBXML_ERROR` in `io-svg.c`, ensuring proper build functionality.
    
    Additionally, the Buildroot package for `libsvgtiny-pixbuf` has been refined. This includes:
    * Updating **dependencies** to reflect `host-libsvgtiny-pixbuf`.
    * Adding logic for **`LIBSVGTINY_PIXBUF_UPDATE_CACHE`** and **`LIBSVGTINY_PIXBUF_POST_INSTALL_TARGET_HOOKS`** to correctly manage `gdk-pixbuf` loader cache, especially when dealing with dynamic libraries.
    
    Signed-off-by: minicx <minicx@disroot.org>

--- build_scripts/buildroot/package/libsvgtiny-pixbuf/libsvgtiny-pixbuf.mk
+++ build_scripts/buildroot/package/libsvgtiny-pixbuf/libsvgtiny-pixbuf.mk
@@ -1,15 +1,32 @@
 ################################################################################
 #
-# Libsvgtiny-pixbuf
+# libsvgtiny-pixbuf
 #
 ################################################################################
 
 LIBSVGTINY_PIXBUF_VERSION = 0.0.2
 LIBSVGTINY_PIXBUF_SITE = https://michael.orlitzky.com/code/releases
-LIBSVGTINY_PIXBUF_SOURCE = libsvgtiny-pixbuf-0.0.2.tar.xz
+LIBSVGTINY_PIXBUF_SOURCE = libsvgtiny-pixbuf-$(LIBSVGTINY_PIXBUF_VERSION).tar.xz
 LIBSVGTINY_PIXBUF_LICENSE = AGPL
 LIBSVGTINY_PIXBUF_LICENSE_FILES = LICENSE
-LIBSVGTINY_PIXBUF_INSTALL_TARGET = YES
-LIBSVGTINY_PIXBUF_DEPENDENCIES = libsvgtiny cairo libxml2 gdk-pixbuf
+
+LIBSVGTINY_PIXBUF_DEPENDENCIES = libsvgtiny cairo libxml2 gdk-pixbuf host-libsvgtiny-pixbuf
+
+HOST_LIBSVGTINY_PIXBUF_DEPENDENCIES = host-libsvgtiny host-cairo host-libxml2 host-gdk-pixbuf
+
+ifeq ($(BR2_STATIC_LIBS),)
+define LIBSVGTINY_PIXBUF_UPDATE_CACHE
+    $(SED) '/^"\/usr\/lib\/gdk-pixbuf-2.0\/2.10.0\/loaders\/libpixbufloader-libsvgtiny\.so"/,/^$$/d' \
+            $(TARGET_DIR)/usr/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache
+	GDK_PIXBUF_MODULEDIR=$(HOST_DIR)/lib/gdk-pixbuf-2.0/2.10.0/loaders \
+		$(HOST_DIR)/bin/gdk-pixbuf-query-loaders | \
+		sed 's,^"lib,"/usr/lib,g' | \
+		sed -n  '/^"\/usr\/lib\/gdk-pixbuf-2.0\/2.10.0\/loaders\/libpixbufloader-libsvgtiny\.so"/,/^$$/p' \
+		>> $(TARGET_DIR)/usr/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache
+endef
+LIBSVGTINY_PIXBUF_POST_INSTALL_TARGET_HOOKS += LIBSVGTINY_PIXBUF_UPDATE_CACHE
+endif
+
 
 $(eval $(autotools-package))
+$(eval $(host-autotools-package))
