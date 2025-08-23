a0b465c08e55c749d1386d7dc21d994a517deca6
minicx <minicx@disroot.org>
2025-08-23 16:03:24 +0300

    buildroot(klipper): bump version to e63a72839da4bd3d143b89fd44fbb495ae1ab7de
    Signed-off-by: minicx <minicx@disroot.org>

--- build_scripts/buildroot/package/klipper/klipper.mk
+++ build_scripts/buildroot/package/klipper/klipper.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KLIPPER_VERSION = 53816dc3b120b04ac078e51db143d8ac094d522a
+KLIPPER_VERSION = e63a72839da4bd3d143b89fd44fbb495ae1ab7de
 KLIPPER_SITE = https://github.com/loss-and-quick/klipper.git
 KLIPPER_SITE_METHOD = git
 
