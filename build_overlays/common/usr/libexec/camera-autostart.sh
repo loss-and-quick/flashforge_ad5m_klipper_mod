5f3bd6829dfc7ea43e1fd8cf77f20a3372ba757f
minicx <minicx@disroot.org>
2025-09-13 12:30:48 +0300

    buildroot(common): Let ustreamer auto-detect camera format and resolution
    Signed-off-by: minicx <minicx@disroot.org>

--- build_overlays/common/usr/libexec/camera-autostart.sh
+++ build_overlays/common/usr/libexec/camera-autostart.sh
@@ -4,43 +4,13 @@
 #
 
 CAMERA_DEVNAME=video0
-CAMERA_SUBSYSTEM=video4linux
-CAMERA_MIN_X_RES=1280
 HTTP_PORT=8080
-
 TAG="camera-autostart"
 PID_FILE=/run/ustreamer.pid
 
 start() {
-    # check supported formats
-    if ! v4l2-ctl -d /dev/$DEVNAME --list-formats | grep "MJPG"
-    then
-        logger -t $TAG "camera /dev/$DEVNAME does not support MJPG"
-        exit 0
-    fi
-
-    # get resolutions, sort so that lowest resolution comes first in list
-    # choose first resolution that is greater or equal CAMERA_MIN_X_RES, or the highest supported resolution else
-
-    resolutions="$(v4l2-ctl -d /dev/$DEVNAME --list-framesizes MJPG | grep -i "discrete" |  cut -d" " -f3 | sort -n)"
-    if [ $? -ne 0 ] || [ -z "$resolutions" ]
-    then
-        logger -t $TAG "failed to get resolutions for /dev/$DEVNAME"
-        exit 0
-    fi
-
-    for res in $resolutions
-    do
-	resx="$(echo "$res" | cut -d'x' -f1)"
-        resolution=$res
-        if [ "$resx" -ge "$CAMERA_MIN_X_RES" ]
-        then
-	    break
-        fi
-    done
-
-    start-stop-daemon -S -b -m -p $PID_FILE --exec ustreamer -- -d /dev/$DEVNAME -r $resolution -m MJPEG --device-timeout=2 -w 1 -I MMAP -c HW -s* -p 8080
-    [ $? -eq 0 ] && logger -t $TAG "started ustreamer for /dev/$DEVNAME with res $resolution" || logger -t $TAG "failed to start ustreamer"
+    start-stop-daemon -S -b -m -p $PID_FILE --exec ustreamer -- -d /dev/$DEVNAME --device-timeout=2 -w 1 -I MMAP -s* -p 8080
+    [ $? -eq 0 ] && logger -t $TAG "started ustreamer for /dev/$DEVNAME" || logger -t $TAG "failed to start ustreamer"
 }
 
 stop() {
