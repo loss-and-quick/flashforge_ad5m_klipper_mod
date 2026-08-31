2695c72e53ccfc0e2696e7a5cf1b108a3e1fc258
minicx <minicx@disroot.org>
2026-09-01 00:07:46 +0300

    fix(buildroot): Add the libevent dependency to guppyscreen
    GuppyScreen links against libevent, but the package only declared
    libevdev and relied on another package pulling libevent in.
    
    Signed-off-by: minicx <minicx@disroot.org>

--- build_scripts/buildroot/package/guppyscreen/guppyscreen.mk
+++ build_scripts/buildroot/package/guppyscreen/guppyscreen.mk
@@ -10,7 +10,7 @@ GUPPYSCREEN_SITE_METHOD = git
 GUPPYSCREEN_GIT_SUBMODULES = yes
 GUPPYSCREEN_LICENSE = GPL-3.0+
 GUPPYSCREEN_LICENSE_FILES = COPYING
-GUPPYSCREEN_DEPENDENCIES = libevdev
+GUPPYSCREEN_DEPENDENCIES = libevdev libevent
 
 define GUPPYSCREEN_BUILD_CMDS
 $(BASH) pushd $(@D)/lv_drivers; patch --forward -p1 < ../patches/0001-lv_driver_fb_ioctls_flashforge_ad5m.patch; popd;
